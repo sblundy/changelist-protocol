32ce22e4ba3c25a03d801e101d2520aae9e95c4a
GitHub Action <action@github.com>
2023-10-24 03:28:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/sblundy/changelistprotocol/MyBundle.kt
+++ src/main/kotlin/com/github/sblundy/changelistprotocol/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sblundy.changelistprotocol
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sblundy/changelistprotocol/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sblundy/changelistprotocol/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sblundy.changelistprotocol.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
